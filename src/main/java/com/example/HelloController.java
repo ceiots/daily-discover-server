6454bff5781e66f853bcd9e36d29e74e432fe60f
ceiots <ceiots@126.com>
2024-11-26 18:49:33 +0800

    提交最新代码，修复访问路径

--- src/main/java/com/example/HelloController.java
+++ src/main/java/com/example/HelloController.java
@@ -1,7 +1,6 @@
 package com.example;
 
 import org.springframework.web.bind.annotation.GetMapping;
-
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,5 +9,10 @@ public class HelloController {
     @GetMapping("/")
     public String hello() {
         return "Hello, World! my";
+    }
+
+    @GetMapping("/api/hello")
+    public String helloApi() {
+        return "Hello from API!";
     }
 }
