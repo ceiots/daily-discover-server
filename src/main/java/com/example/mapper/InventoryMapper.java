cefb05dc2b60c5da5ba89179ee5d82842c9440ff
ceiots <ceiots@126.com>
2025-06-11 01:58:08 +0800

    refactor: 移除 daily-discover-common 模块
    - 删除了 daily-discover-common/pom.xml 文件
    - 删除了 daily-discover-common/src/main/java/ 下的所有包和类
    - 在 daily-discover-user/pom.xml 中添加了 JWT 相关依赖
    - 删除了 src/main/java/com/example/controller/ 下的所有控制器类

--- src/main/java/com/example/mapper/InventoryMapper.java
+++
@@ -1,9 +0,0 @@
-package com.example.mapper;
-
-import org.apache.ibatis.annotations.Update;
-
-public interface InventoryMapper {
-
-    @Update("UPDATE inventory SET stock = stock - #{quantity} WHERE product_id = #{productId} AND stock >= #{quantity}")
-    int decreaseStock(Long productId, int quantity);
-} 
